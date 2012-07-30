fb81a93082bafc37614b8c99d42ed3d7a11c67ed
Pawel Pierzchala <pawel.pierzchala@llp.pl>
2012-07-30 22:12:47 +0200

    ignores tests tagged with wip on travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,6 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.infer_base_class_for_anonymous_controllers = false
+  config.treat_symbols_as_metadata_keys_with_true_values = true
   config.order = "random"
 end
